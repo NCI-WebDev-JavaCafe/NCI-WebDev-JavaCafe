d70df8eea69089b72974a5ef6af93d8fba863bc5
tdorigan <dorigan.tiago@gmail.com>
2024-11-02 17:46:41 +0000

    add footer, in a separated file

---
+++ js/footer.js
@@ -0,0 +1,15 @@
+
+// Wait for the entire DOM to load before running this script
+document.addEventListener("DOMContentLoaded", function () {
+
+  // Fetch the HTML content from 'footer.html' and insert it into the footer container
+  fetch("./footer.html")
+    .then(response => response.text()) // Convert the response to plain text (HTML content)
+    .then(data => {
+
+      // Insert the fetched HTML into the <div id="footer-container">
+      document.getElementById("footer-container").innerHTML = data;
+
+
+    });
+});
